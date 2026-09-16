7778dd6f5e5147d85b9cb7f7ccc57df3845d1825
zhurykmaks11 <zhurik.max@gmail.com>
2026-09-16 17:16:44 +0300

    added prisma scema

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { createObserveModule } from '@nestjs/observe';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
@@ -14,6 +15,7 @@ export const { ObserveModule, ObserveInstrument } = createObserveModule();
       appSecret: 'YOUR_APP_SECRET',
       serviceId: 'backend',
     }),
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
